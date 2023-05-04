608417aedc59c9979892a1e3881d7f4a54638dcd
Riley Blackwell <rileyblackwell5@gmail.com>
2023-05-03 21:16:47 -0400

    improved parser

--- web_scrapping_demo/parse_web_page.py
+++ web_scrapping_demo/parse_web_page.py
@@ -4,20 +4,23 @@ def parse(f, *args):
     data = ''
     store_data = False
     for i in range(len(f)-3):
-        tag = f[i:i+15]
-        if store_data and '<' not in tag and '/' not in tag:
+        tag = f[i:i+len(args[0])]
+        if store_data and '</' not in tag:
             try:
-                data += str(int(tag[14:]))
+                data += str(int(tag[len(args[0]) - 1:]))
             except ValueError:
-                if tag[14:] == '.':
-                    data += tag[14:]    
-        if tag == args[0]:
+                if tag[len(args[0]) - 1:] == '.':
+                    data += tag[len(args[0]) - 1:]    
+        if tag == args[0] or tag == args[1]:
             store_data = True
-        if args[1] in tag:
+        if args[2] in tag:
             store_data = False
-            if tag[:2] == '</':    
+            if tag[:len(args[2])] == '</span></td>':    
                 data += ', '         
-    return data
+    return data[:-2]
 
-print(parse(get_web_page('https://www.espn.com/nfl/team/stats/_/name/dal/dallas-cowboys'), '<span class=\"\">', '</span>'))
- 
+print(parse(get_web_page('https://www.espn.com/nhl/team/stats/_/name/bos/boston-bruins'), 
+            '<td class=\"Table__TD\"><span class=\"\">', 'span class=\"Stats__TotalRow fw-bold\">', '</span></td>'))
+
+# f = open('sample_bengals_data.txt', 'r').read() 
+# print(parse(f, '<td class=\"Table__TD\"><span class=\"\">', '</span>'))
