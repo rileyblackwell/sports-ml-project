540b8b8f9d21009762b6a57f28495b22a58dbcd8
Riley Blackwell <rileyblackwell5@gmail.com>
2023-08-10 16:24:31 -0400

    reduced training loss of NN

--- web_scrapper/randomize_player_urls.py
+++ web_scrapper/randomize_player_urls.py
@@ -3,7 +3,8 @@ import random
 if __name__ == '__main__':
     with open('player_urls.out', 'r') as f:
         players = f.readlines()
-        random.shuffle(players)
+        for _ in range(100000):
+            random.shuffle(players)
         with open('player_urls.out', 'w') as out:
             for player in players:
                 out.write(player)
