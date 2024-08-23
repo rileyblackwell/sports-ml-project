2c329f83e6ee9a66dd401b065c57215d7783ee3b
Riley Blackwell <rileyblackwell5@gmail.com>
2024-08-23 18:33:20 -0400

    fix: team_id/main.py now produces the correct result for rbs and wrs

--- web_scrapper/team_id/main.py
+++ web_scrapper/team_id/main.py
@@ -1,7 +1,7 @@
 import sqlite3
 from html.parser import HTMLParser
 
-class PlayerTeamIdParser(HTMLParser):
+class PlayerTeamIdRBParser(HTMLParser):
     def __init__(self):
         super().__init__()     
         self.in_td_tag = False
@@ -23,6 +23,28 @@ class PlayerTeamIdParser(HTMLParser):
                 self.teams.append(f"{data}, ") 
             self.counter += 1 
 
+class PlayerTeamIdWRParser(HTMLParser):
+    def __init__(self):
+        super().__init__()     
+        self.in_td_tag = False
+        self.in_span_tag = False
+        self.teams = []
+        self.counter = 0
+         
+    def handle_starttag(self, tag, attrs):         
+        if tag == 'td':
+            self.in_td_tag = True
+         
+    def handle_endtag(self, tag):
+        if tag == 'td':
+            self.in_td_tag = False    
+
+    def handle_data(self, data):
+        if self.in_td_tag:
+            if self.counter % 9 == 0 or self.counter % 9 == 1:
+                self.teams.append(f"{data}, ") 
+            self.counter += 1
+             
 def get_player_stats_html_from_db(player):
     conn = sqlite3.connect("../player_stats.db")
     cursor = conn.cursor()
@@ -46,11 +68,11 @@ def write_player_team_id_to_db(player, team_id):
     
     conn.close()
 
-def get_player_urls_from_db():
+def get_player_urls_from_db(position):
     conn = sqlite3.connect("../player_stats.db")
     cursor = conn.cursor()
 
-    cursor.execute("SELECT player_url FROM player_urls")
+    cursor.execute("SELECT player_url FROM player_positions WHERE data = ?", (position,))
     results = cursor.fetchall()
     
     conn.close()
@@ -58,9 +80,37 @@ def get_player_urls_from_db():
     return [result[0] for result in results]
 
 def main():
-    parser = PlayerTeamIdParser()
-    player_urls = get_player_urls_from_db()
-    for player in player_urls:
+    # Process RB players
+    parser = PlayerTeamIdRBParser()
+    rb_player_urls = get_player_urls_from_db("rb")
+    for player in rb_player_urls:
+        stats_html = get_player_stats_html_from_db(player)
+        if stats_html:
+            parser.feed(stats_html)
+        else:
+            print(f"No stats found for player {player}")
+            continue
+        
+        teams = parser.teams[:len(parser.teams) // 2]
+        if len(teams) % 2 == 1:
+            teams = parser.teams[:(len(parser.teams) + 2) // 2]
+      
+        # outputs the team id for the last 3 seasons   
+        if len(teams) >= 8:
+            team_id = ''.join(teams[-10:-4]) # teams played for 2020, 2021, 2022
+        elif len(teams) == 6:
+            team_id = ''.join(teams[-8:-4])
+        elif len(teams) == 4:
+            team_id = ''.join(teams[-6:-4])
+        
+        write_player_team_id_to_db(player, team_id)
+        parser.counter = 0 # resets the counter for the next player
+        parser.teams.clear() # clears the list for the next player
+
+    # Process WR players
+    parser = PlayerTeamIdWRParser()
+    wr_player_urls = get_player_urls_from_db("wr")
+    for player in wr_player_urls:
         stats_html = get_player_stats_html_from_db(player)
         if stats_html:
             parser.feed(stats_html)
